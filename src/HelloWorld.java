1b9c9fee6e0ddaa13c9716ac550979237e6a135b
Labone <labs345@gmail.com>
2026-07-02 14:16:58 +0530

    fetch command practice

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
         System.out.println("Hello world");
         System.out.println("changes from new branch");
         System.out.println("changes from github");
+         System.out.println("changes for fetch");
     }
 }
